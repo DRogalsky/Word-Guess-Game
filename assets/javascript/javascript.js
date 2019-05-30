057bfa622a60efa1b8fb7f12644df9857cb90421
dnl092 <drogalsky14@my.whitworth.edu>
2019-05-30 12:32:52 -0700

    added a word selector and started the if statement to see if they used the letter before

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,23 @@
-//TODO: log keys
+//TODO: chooes words?
+var possibleWords = ['grip','breathe','blue','tiger','follow','shrill', 'clever', 'structure', 'rainstorm', 'egg', 'sow', 'resolve', 'smite', 'defiant', 'scientific', 'machine', 'play', 'persuade', 'push'];
+function newWord() {
+    let selector = Math.floor(Math.random()*possibleWords.length);
+    return possibleWords[selector];
+}
+console.log(newWord());
+
+document.onkeyup = function(event) {
+    let key = event.key;
+    let guessed = [];
+    //TODO: function to check the letter
+    if (guessed.includes(key)) {
+        // youve guessed this! probbably alert?
+    } else {
+        guessed.push(key);
+        //call the function i haven't made yet
+    }
+}
 //TODO: check against word
 //TODO: make letters show up on screen
 //TODO: if word is complete win if hangman complete lose
-//TODO: 
+//TODO: keep track of chosen letters?
