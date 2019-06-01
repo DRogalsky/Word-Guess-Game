750f21d4767e3ce7fd84bbd8321b35efa009bab6
dnl092 <drogalsky14@my.whitworth.edu>
2019-05-31 18:53:17 -0700

    got the snowman working and made a restart button instead of it being automatic

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,6 +10,17 @@ let wordBox = document.getElementById('wordBox');
 let guessedLettersBox = document.getElementById('guessedLettersBox');
 let winsBox = document.getElementById('winsBox');
 let lossesBox = document.getElementById('lossesBox');
+let snowBody = document.getElementById('body');
+let snowHead = document.getElementById('head');
+let snowLeftEye = document.getElementById('left');
+let snowRightEye = document.getElementById('right');
+let snowMouth = document.getElementById('mouth');
+let snowNose = document.getElementById('nose');
+let snowScarf = document.getElementById('scarf');
+let abominable = document.getElementById('abominable');
+let snowArm = document.getElementById('arms');
+let restart = document.getElementById('restart');
+
 winsBox.textContent = wins;
 lossesBox.textContent = losses;
 
@@ -39,6 +50,15 @@ function cleanUp() {
     }
     wordBox.textContent = showTheWord(theWord);
     guessedLettersBox.textContent = guessed;
+    snowBody.style.visibility = 'hidden';
+    snowHead.style.visibility = 'hidden';
+    snowArm.style.visibility = 'hidden';
+    snowLeftEye.style.visibility = 'hidden';
+    snowRightEye.style.visibility = 'hidden';
+    snowMouth.style.visibility = 'hidden';
+    snowScarf.style.visibility = 'hidden';
+    snowNose.style.visibility = 'hidden';
+    abominable.style.visibility = 'hidden';
 }
 
 function letterCheck(letter) {
@@ -66,21 +86,49 @@ document.onkeyup = function(event) {
             wordBox.textContent = showTheWord(theWord);
             if (checker === false) {
                 wrong++;
+                if (wrong === 1) {
+                    snowBody.style.visibility = 'visible';
+                } else if(wrong === 2) {
+                    snowHead.style.visibility = 'visible';
+                } else if(wrong === 3) {
+                    snowArm.style.visibility = 'visible';
+                } else if(wrong === 4) {
+                    snowLeftEye.style.visibility = 'visible';
+                } else if(wrong === 5) {
+                    snowNose.style.visibility = 'visible';
+                } else if(wrong === 6) {
+                    snowRightEye.style.visibility = 'visible';
+                } else if(wrong === 7) {
+                    snowMouth.style.visibility = 'visible';
+                } else if(wrong === 8) {
+                    snowScarf.style.visibility = 'visible';
+                } else if(wrong === 9) {
+                    snowBody.style.visibility = 'hidden';
+                    snowHead.style.visibility = 'hidden';
+                    snowArm.style.visibility = 'hidden';
+                    snowLeftEye.style.visibility = 'hidden';
+                    snowRightEye.style.visibility = 'hidden';
+                    snowMouth.style.visibility = 'hidden';
+                    snowScarf.style.visibility = 'hidden';
+                    snowNose.style.visibility = 'hidden';
+                    abominable.style.visibility = 'visible';
+                    losses++;
+                    lossesBox.textContent = losses;
+                }
+
             }
             if (theWord.indexOf('_') === -1) {
                 alert('YOU WIN');
                 wins++;
                 winsBox.textContent = wins;
-                cleanUp();
-            }
-            if (wrong === 9) {
-                alert('YOU LOSE');
-                losses++;
-                lossesBox.textContent = losses;
-                cleanUp();
             }
         }
     }
+}
+
+
+window.onload = function() {
+    restart.onclick = cleanUp;
 }
 
 cleanUp();
